4d59e0e6e5b97a400090053438b2ed3ecb4c3029
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-11-17 10:57:52 +0000

    load sensor log history on startup
    This makes it so that the server can restart without losing track
    of the 24-hour min/max temperature stats.

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -22,40 +25,134 @@ var sensorReportRE = regexp.MustCompile(
 		`([^,]+),\s*` + // Temperature F (float)
 		`([^,]+)`) // Monotonic increasing timestamp check: "OK" or "DUP"
 
+// Type for managing sensor report histories of multiple sensor nodes
+type NodeHistories map[string]*ReportHistory
+
+// Read sensor log files to get reports from the past `days` number of days
+func ReadSensorLogHistory(days int) (NodeHistories, error) {
+	if days < 0 {
+		return nil, fmt.Errorf("ERROR: Expected days >= 0, got: %d", days)
+	}
+	log.Printf("INFO: Loading sensor node report history from CSV logs")
+	histories := make(NodeHistories)
+
+	// Read logs for the past `days` number of days, oldest log first
+	for i := days - 1; i >= 0; i-- {
+		path, err := getLogFilePathForTodayPlus(-i)
+		if err != nil {
+			return nil, fmt.Errorf(
+				"ERROR: Generating file path for %d days ago: %v", i, err)
+		}
+
+		// Open the log file
+		file, err := os.Open(path)
+		if err != nil {
+			// This is fine. For example, maybe there is only the current
+			// day's sensor data available.
+			log.Printf("WARN: %v", err)
+			continue
+		}
+		reader := csv.NewReader(file)
+		log.Printf("INFO: Loading %s", path)
+
+		// Skip CSV header row
+		if _, err := reader.Read(); err != nil {
+			log.Printf("WARN: Reading CSV header row: %v", err)
+			// If the file is empty, that's fine. Skip this file.
+			file.Close()
+			continue
+		}
+
+		// Parse CSV data rows
+		// CAUTION: This attempts to continue after parsing errors
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("WARN: Reading CSV record: %v", err)
+				continue
+			}
+
+			// Parse record (format: Timestamp,Node,RSSI,SNR,BatteryV,TempF)
+			timestamp, err := time.Parse(time.RFC3339, record[0])
+			if err != nil {
+				log.Printf("WARN: Parsing timestamp: %v", err)
+
+			}
+			node := record[1]
+			// ignore: rssi := record[2]
+			// ignore: snr := record[3]
+			batteryV, err := strconv.ParseFloat(record[4], 64)
+			if err != nil {
+				log.Printf("WARN: Parsing batteryV: %v", err)
+			}
+			tempF, err := strconv.ParseFloat(record[5], 64)
+			if err != nil {
+				log.Printf("WARN: Parsing tempF: %v", err)
+			}
+
+			// Ensure history exists for this node
+			h, exists := histories[node]
+			if !exists {
+				h = &ReportHistory{}
+				histories[node] = h
+			}
+
+			// Add the data to the history for this node
+			h.Add(timestamp, batteryV, tempF)
+		}
+		// Close this log file
+		file.Close()
+	}
+
+	return histories, nil
+}
+
+// Format an IRC summary message for the most recent report of nodes 1 and 2
+func FormatReportSummary(histories NodeHistories) string {
+	lines := []string{}
+
+	for _, nodeID := range []string{"1", "2"} {
+		h, exists := histories[nodeID]
+		if !exists || len(h.Reports) == 0 {
+			// No data yet for this node
+			lines = append(lines, "/--/--")
+			continue
+		}
+		last := h.Reports[len(h.Reports)-1]
+		// Format timestamp like "Nov15 05:30"
+		timestampStr := last.Timestamp.Format("Jan02 15:04")
+		lines = append(lines,
+			fmt.Sprintf("/%.0f %.0f %.0f %.0f/  %s  %s",
+				last.TempF, 100*last.BatteryV, h.MinTempF, h.MaxTempF,
+				nodeID, timestampStr))
+	}
+
+	// Return string in "!pre /..." format for irc-display-bot
+	return "!pre " + strings.Join(lines, "")
+}
+
 // main() reads serial sensor reports, maintains a 24-hour rolling history per
-// node, and generates IRC bot summaries for nodes IDs 1 and 2.
-//
-// Sensor reports are expected in this format (NOTE: timestamp should be
-// monotonic increasing, but don't trust it to accurately reflect clock time):
-//
-//	<PROTOCOL>: <RSSI>, <SNR>, <NODE_ID>, <TIMESTAMP>, <BATTERY_V>, <TEMP_F>, <OK/DUP>
-//
-// Example reports:
+// node, and sends report summaries by IRC (sets topic of configured channel).
+// Example sensor reports (expected format of USB serial stream):
 //
 //	LORA: -122, -14.0, 1, 38734ca6, 3.80, 63, DUP
 //	ESPNOW: -63, 0.0, 2, 38734b3c, 3.80, 64, OK
 //
-// Each new report for a node updates its rolling 24-hour history, including
-// minimum and maximum temperatures observed.
+// IRC topic summaries use `!pre /...` format for multi-line formatted output
+// on my IRC display bot (see https://github.com/samblenny/irc-display-bot).
+// Example of a formatted 4-line summary to work with irc-display-bot:
 //
-// After adding a report, this generates a summary message for the IRC bot to
-// send. The summary format uses the `!pre /...` syntax for my IRC display bot
-// (see https://github.com/samblenny/irc-display-bot). The format uses
-// delimeters to expand one line into up to 4 lines in the format:
+//	!pre /63 368 63 86/  1  Nov16 23:43/66 376 66 93/  2  Nov17 23:43
 //
-//	!pre /<line1>/<line2>/<line3>/<line4>
+// After the linebreak substitutions, that would appear on a screen as:
 //
-// The expanded format is:
-//
-//	<line1>: <node1.TempF> <node1.Centivolts> <node1.MinTempF> <node1.MaxTempF>
-//	<line2>:   <node1 ID>  <node1 timestamp of last report>
-//	<line3>: <node2.TempF> <node2.Centivolts> <node2.MinTempF> <node2.MaxTempF>
-//	<line4>:   <node2 ID>  <node2 timestamp of last report>
-//
-// When no data is available for a node, those lines get a "--" placeholder.
-// Example summary format sent to the IRC bot:
-//
-//	!pre /63 379 63 63/ 1  Nov15 01:59/67 389 67 67/ 2  Nov15 01:58
+//	63 368 63 86
+//	  1  Nov16 23:43
+//	66 376 66 93
+//	  2  Nov16 23:43
 func main() {
 	log.Printf("INFO: Starting serial-sensor-hub")
 
@@ -74,14 +171,39 @@ func main() {
 	reportChan := make(chan string, 32)
 	sensorLogChan := make(chan SensorData, 32)
 
+	// Try to initialize sensor node report history from recent log files.
+	// Node histories get used to compute 24-hour rolling min/max temperatures.
+	histories, err := ReadSensorLogHistory(2)
+	if err != nil {
+		// Loading the old log data failed, so start from a clean slate
+		log.Print(err)
+		histories = make(NodeHistories)
+	} else {
+		// Loading log data worked, so count how much we got
+		totalReports := 0
+		totalNodes := len(histories)
+		for _, history := range histories {
+			totalReports += len(history.Reports)
+		}
+		log.Printf("INFO: Sensor Log Summary: %d nodes, %d reports",
+			totalNodes, totalReports)
+	}
+
+	// Start IRC bot goroutine (takes several seconds to connect and join)
+	go IRCBot(ctx, cfg, reportChan)
+
+	// Send summary of logged sensor reports for nodes 1 and 2 by IRC
+	if len(histories) > 0 {
+		// First allow time for IRC connect/register/join finish
+		time.Sleep(8 * time.Second)
+		// Okay, now send it
+		summary := FormatReportSummary(histories)
+		reportChan <- summary
+	}
+
 	// Start serial port sensor monitor, IRC bot, and sensor data logger
 	go SerialConnect(sensorChan)
-	go IRCBot(ctx, cfg, reportChan)
 	go StartLogger(sensorLogChan)
-
-	// This map maintains a ReportHistory for each nodeID to compute 24-hour
-	// rolling min/max temperature statistics
-	histories := make(map[string]*ReportHistory)
 
 	// Start sensorChan fanout to sensor log and reportChan channel
 	for report := range sensorChan {
@@ -123,27 +245,8 @@ func main() {
 		timestamp := time.Now()
 		h.Add(timestamp, batteryV, tempF)
 
-		// Prepare summary for nodes 1 and 2
-		lines := []string{}
-
-		for _, nodeID := range []string{"1", "2"} {
-			h, exists := histories[nodeID]
-			if !exists || len(h.Reports) == 0 {
-				// No data yet for this node
-				lines = append(lines, "/--/--")
-				continue
-			}
-			last := h.Reports[len(h.Reports)-1]
-			// Format timestamp like "Nov15 05:30"
-			timestampStr := last.Timestamp.Format("Jan02 15:04")
-			lines = append(lines,
-				fmt.Sprintf("/%.0f %.0f %.0f %.0f/  %s  %s",
-					last.TempF, 100*last.BatteryV, h.MinTempF, h.MaxTempF,
-					nodeID, timestampStr))
-		}
-
-		// Send report summary by IRC
-		summary := "!pre " + strings.Join(lines, "")
+		// Send summary of latest reports for nodes 1 and 2 by IRC
+		summary := FormatReportSummary(histories)
 		reportChan <- summary
 
 		// Log the report to disk
